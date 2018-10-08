8f6ba4f5955b361e4be14600242ccb2c955d825a
aliciaxw <alicia.xinran.wang@gmail.com>
2018-10-07 21:47:05 -0400

    Add chronicle logging keys

---
+++ src/utils/Keys.js
@@ -0,0 +1,7 @@
+const accessKey = `${process.env.ACCESS_KEY || 'undefined access key'}`;
+const secretKey = `${process.env.SECRET_KEY || 'undefined secret key'}`;
+
+module.exports = {
+    accessKey,
+    secretKey,
+};
